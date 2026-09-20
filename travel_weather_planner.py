99d827186207f748dc257cd2d51f3a406f05a19c
crummblet <mansoor.aqeel@hotmail.com>
2026-09-20 12:40:49 -0600

    Add travel_weather_planner.py

---
+++ travel_weather_planner.py
@@ -0,0 +1,18 @@
+# declare variables
+distance_mi = 5.4
+is_raining = False
+has_bike = True
+has_car = True
+has_ride_share_app = True
+
+# conditional statements
+if not distance_mi:
+    print("False")
+elif distance_mi <= 1 and not is_raining:
+    print("True")
+elif distance_mi > 1 and distance_mi <= 6 and has_bike and not is_raining:
+    print("True")
+elif distance_mi > 6 and (has_car or has_ride_share_app):
+    print("True")
+else:
+    print("False")
